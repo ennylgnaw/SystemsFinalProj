f45ce0eb2640aebded04d49ce755651f5ff763af
ennylgnaw <lwang7@stuy.edu>
2019-01-11 11:23:09 -0500

    Added board

--- board.c
+++ board.c
@@ -27,17 +27,49 @@ void generatebombs(char * b[][SIDE]) {
         }
   }
 }
+
 int countbombs(char * b[][],int row,int col){
+  int counter = 0;
   char n = b[row - 1][col];
+  if(n == "*"){
+    counter += 1;
+  }
   char s = b[row + 1][col];
+  if(s == "*"){
+    counter += 1;
+  }
   char e = b[row][col-1];
+  if(e == "*"){
+    counter += 1;
+  }
   char w = b[row][col + 1];
+  if(w == "*"){
+    counter += 1;
+  }
   char ne = b[row - 1][col - 1];
+  if(ne == "*"){
+    counter += 1;
+  }
   char nw = [row - 1][col + 1];
+  if(nw == "*"){
+    counter += 1;
+  }
+
   char se = [row + 1][ col - 1];
+  if(se == "*"){
+    counter += 1;
+  }
   char sw = [row + 1][col + 1];
-  
+  if(sw == "*"){
+    counter += 1;
+  }
+  return counter;
+
 }
+
+
+
+
 main(){
   char answerboard[24][24];
   return 0;
