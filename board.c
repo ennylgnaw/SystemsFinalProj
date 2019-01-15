e7fc0f3939326ccd2aac1107ba140c536bf256f7
ennylgnaw <lwang7@stuy.edu>
2019-01-14 22:42:29 -0500

    Modified file

--- board.c
+++ board.c
@@ -8,91 +8,105 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <time.h>
 
 
 #define SIDE 24
 #define BOMB 99
 
+char **create_2d_array (int colums, int rows) {
+  char **ptr;
+  int i;
+     
+  for (i = 0; i < rows; i++)
+    {
+      ptr [i] = (char *) malloc (colums);
+    }
+ 
+  return ptr;
+}
 
-void generatebombs(char * b[][SIDE]) {
-  int x = 0;
-  int numofbombs = 99;
-  srand(time(NULL));
-  while(numofbombs){
+  void generatebombs(char **b[][SIDE]) {
+    int x = 0;
+    int numofbombs = 99;
+    srand(time(NULL));
+    while(numofbombs){
+      for(int r = 0; r < SIDE; r++)
+	for(int c = 0; c < SIDE; c++)
+	  if (rand()%17 == 1 && strcmp(b[r][c], "_") == 0) {
+	    b[r][c] = "*";
+	    numofbombs--;
+	  }
+    }
+  }
+
+  char countbombs(char **b[][SIDE],int row,int col){
+    int counter = 0;
+    char * n = b[row - 1][col];
+    if(strcmp(n, "*") == 0){
+      counter += 1;
+    }
+    char * s = b[row + 1][col];
+    if(strcmp(s, "*") == 0){
+      counter += 1;
+    }
+    char * e = b[row][col-1];
+    if(strcmp(e, "*") == 0){
+      counter += 1;
+    }
+    char * w = b[row][col + 1];
+    if(strcmp(w, "*") == 0){
+      counter += 1;
+    }
+    char * ne = b[row - 1][col - 1];
+    if(strcmp(ne, "*") == 0){
+      counter += 1;
+    }
+    char * nw = b[row - 1][col + 1];
+    if(strcmp(nw, "*") == 0){
+      counter += 1;
+    }
+
+    char * se = b[row + 1][ col - 1];
+    if(strcmp(se, "*") == 0){
+      counter += 1;
+    }
+    char * sw = b[row + 1][col + 1];
+    if(strcmp(sw, "*") == 0){
+      counter += 1;
+    }
+    return (char) counter;
+
+  }
+
+  void generateboard(char **b[][SIDE]) {
     for(int r = 0; r < SIDE; r++)
       for(int c = 0; c < SIDE; c++)
-	if (rand()%17 == 1 && strcmp(b[r][c], "_") == 0) {
-	  b[r][c] = "*";
-	  numofbombs -= 1;
-        }
-  }
-}
-
-char countbombs(char * b[][SIDE],int row,int col){
-  int counter = 0;
-  char n = b[row - 1][col];
-  if(strcmp(n, "*") == 0){
-    counter += 1;
-  }
-  char s = b[row + 1][col];
-  if(strcmp(s, "*") == 0){
-    counter += 1;
-  }
-  char e = b[row][col-1];
-  if(strcmp(e, "*") == 0){
-    counter += 1;
-  }
-  char w = b[row][col + 1];
-  if(strcmp(w, "*") == 0){
-    counter += 1;
-  }
-  char ne = b[row - 1][col - 1];
-  if(strcmp(ne, "*") == 0){
-    counter += 1;
-  }
-  char nw = b[row - 1][col + 1];
-  if(strcmp(nw, "*") == 0){
-    counter += 1;
+	if (strcmp(b[r][c], "*") != 0)
+	  *b[r][c] = countbombs(b, r, c);
   }
 
-  char se = b[row + 1][ col - 1];
-  if(strcmp(se, "*") == 0){
-    counter += 1;
+  void printboard(char **b[][SIDE]) {
+    int r, c;
+    printf("    ");
+    for(int r = 0; r < SIDE; r++)
+      printf("%d ", r);
+    printf("\n\n");
+    for(int r = 0; r < SIDE; r++) {
+      printf("%d ", r);
+      for(c = 0; c < SIDE; c++)
+	printf("%s ", b[r][c]);
+      printf("\n");
+    }
   }
-  char sw = b[row + 1][col + 1];
-  if(strcmp(sw, "*") == 0){
-    counter += 1;
+
+
+  int main(){
+    printf("1\n");
+    char **aboard[SIDE][SIDE] = create_2d_array(SIDE, SIDE);
+    printf("Hello");
+    generatebombs(aboard);
+    generateboard(aboard);
+    printboard(aboard);
+    return 0;
   }
-  return (char) counter;
-
-}
-
-void generateboard(char *b[][SIDE]) {
-  for(int r = 0; r < SIDE; r++)
-    for(int c = 0; c < SIDE; c++)
-      if (strcmp(b[r][c], "*") != 0)
-	b[r][c] = countbombs(b, r, c);
-}
-
-void printboard(char *b[][SIDE]) {
-  int r, c;
-  printf("    ");
-  for(int r = 0; r < SIDE; r++)
-    printf("%d ", r);
-  printf("\n\n");
-  for(int r = 0; r < SIDE; r++) {
-    printf("%d ", r);
-    for(c = 0; c < SIDE; c++)
-      printf("%s ", b[r][c]);
-    printf("\n");
-  }
-}
-
-
-main(){
-  char * aboard[24][24];
-  generatebombs(aboard);
-  generateboard(aboard);
-  printboard(aboard);
-  return 0;
-}
