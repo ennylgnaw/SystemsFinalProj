e06fd52610d33265b4297a79c368d8f4b38eb42f
ennylgnaw <lwang7@stuy.edu>
2019-01-17 03:09:43 -0500

    Added file

--- board.c
+++ board.c
@@ -20,79 +20,115 @@ void generatebombs(char *b[][SIDE]) {
   while (numofbombs) {
     int x = rand() % SIDE;
     int y = rand() % SIDE;
-    b[x][y] = "*";
+    if (strcmp(b[x][y], "_") == 0)
+      b[x][y] = "*";
     numofbombs--;
   }
 }
 
-char countbombs(char *b[][SIDE],int row,int col){
+int countbombs(char *b[][SIDE],int row,int col){
   int counter = 0;
-  char * n = b[row - 1][col];
-  if(strcmp(n, "*") == 0){
-    counter += 1;
-  }
-  char * s = b[row + 1][col];
-  if(strcmp(s, "*") == 0){
-    counter += 1;
-  }
-  char * e = b[row][col-1];
-  if(strcmp(e, "*") == 0){
-    counter += 1;
-  }
-  char * w = b[row][col + 1];
-  if(strcmp(w, "*") == 0){
-    counter += 1;
-  }
-  char * ne = b[row - 1][col - 1];
-  if(strcmp(ne, "*") == 0){
-    counter += 1;
-  }
-  char * nw = b[row - 1][col + 1];
-  if(strcmp(nw, "*") == 0){
-    counter += 1;
+  if (row != 0) {
+    char * n = b[row - 1][col];
+    if(strcmp(n, "*") == 0){
+      counter += 1;
+    }
   }
 
-  char * se = b[row + 1][ col - 1];
-  if(strcmp(se, "*") == 0){
-    counter += 1;
+  if (row != SIDE - 1) {
+    char * s = b[row + 1][col];
+    if(strcmp(s, "*") == 0){
+      counter += 1;
+    }
   }
-  char * sw = b[row + 1][col + 1];
-  if(strcmp(sw, "*") == 0){
-    counter += 1;
+
+  if (col != 0) {
+    char * e = b[row][col-1];
+    if(strcmp(e, "*") == 0){
+      counter += 1;
+    }
   }
-  return (char) counter;
+
+  if (col != SIDE - 1) {
+    char * w = b[row][col + 1];
+    if(strcmp(w, "*") == 0){
+      counter += 1;
+    }
+  }
+
+  if (! (row == 0 || col == 0)) {
+    char * ne = b[row - 1][col - 1];
+    if(strcmp(ne, "*") == 0){
+      counter += 1;
+    }
+  }
+
+  if (! (row == 0 || col == SIDE - 1)) {
+    char * nw = b[row - 1][col + 1];
+    if(strcmp(nw, "*") == 0){
+      counter += 1;
+    }
+  }
+
+  if (! (row == SIDE - 1 || col == 0)) {
+    char * se = b[row + 1][ col - 1];
+    if(strcmp(se, "*") == 0){
+      counter += 1;
+    }
+  }
+
+  if (! (row == SIDE - 1 || col == SIDE - 1)) {
+    char * sw = b[row + 1][col + 1];
+    if(strcmp(sw, "*") == 0){
+      counter += 1;
+    }
+  }
+
+  return counter;
 
 }
 
 void generateboard(char *b[][SIDE]) {
   for(int r = 0; r < SIDE; r++)
     for(int c = 0; c < SIDE; c++)
-      if (strcmp(b[r][c], "*") != 0)
-	*b[r][c] = countbombs(b, r, c);
+      b[r][c] = "_";
+}
+
+void addbombcount(char *b[][SIDE]) {
+  for(int r = 0; r < SIDE; r++)
+    for(int c = 0; c < SIDE; c++)
+      if (strcmp(b[r][c], "*") != 0) {
+	*b[r][c] = (char) countbombs(b, r, c);
+      }
 }
 
 void printboard(char *b[][SIDE]) {
   int r, c;
   printf("    ");
-  for(int r = 0; r < SIDE; r++)
-    printf("%d ", r);
+  for(c = 0; c < SIDE; c++)
+    printf("%-4d", c);
   printf("\n\n");
-  for(int r = 0; r < SIDE; r++) {
-    printf("%d ", r);
+  for(r = 0; r < SIDE; r++) {
+    printf("%-4d", r);
     for(c = 0; c < SIDE; c++)
-      printf("%s ", b[r][c]);
-    printf("\n");
+      printf("%-4s", b[r][c]);
+    printf("\n\n");
   }
 }
 
 
 int main(){
   char *aboard[SIDE][SIDE];
-  generatebombs(aboard);
-  printf("Done\n");
   generateboard(aboard);
-    /*
+  generatebombs(aboard);
   printboard(aboard);
-  return 0;
+  printf("%d\n", countbombs(aboard, 0, 0));
+  printf("%d\n", countbombs(aboard, 12, 12));
+  addbombcount(aboard);
+  /*
+    countbombs(aboard, 12, 12);
+    generateboard(aboard);
+    printboard(aboard);
+    return 0;
   */
 }
